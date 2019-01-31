6d5515b7aa9282ea2227843e4f85de73b803602b
Ilgu Lee <iglee2000ca@gmail.com>
2019-01-31 07:30:58 -0500

    Using functions expression body and making code more readable with when block

---
+++ src/section1/section1.1.kt
@@ -0,0 +1,25 @@
+package section1
+
+/**
+ * Main function is used to call the min() function
+ * with the two values provided as arguments
+ */
+fun main(args: Array<String>) {
+    val firstValue = 10
+    val secondValue = 20
+    println("Smallest value is: ${min(firstValue, secondValue)}")
+}
+
+/**
+ * This function is implemented with a block body.
+ * Replace the block body with an expression body.
+ */
+//fun min(value1: Int, value2: Int): Int {
+//    if (value1 < value2){
+//        return value1
+//    } else {
+//        return value2
+//    }
+//}
+fun min(value1: Int, value2: Int): Int = if (value1 < value2) value1 else value2
+
