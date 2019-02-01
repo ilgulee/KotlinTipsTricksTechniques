8068e983f98b50457bbe48eaf2d6e847acdf3e45
Ilgu Lee <iglee2000ca@gmail.com>
2019-02-01 08:39:52 -0500

    Implicit and Explicit lambda

---
+++ src/section4/section4.1a.kt
@@ -0,0 +1,24 @@
+package section4
+
+/**
+ * This exercise will compare lambdas against functions.
+ * First the function sum will be executed. And then it
+ * will be replaced by a lambda with the same functionality
+ */
+fun main(args: Array<String>) {
+    // Create a lambda expression to replace sum()
+    // and return the result to the value sum
+//    val sum={value1:Int,value2:Int->value1+value2}
+    val sum: (Int, Int) -> Int = { value1, value2 -> value1 + value2 }
+    val total = sum(5, 3)
+    println(total)
+}
+
+/**
+ * This function is used at the start of the
+ * exercise for demonstration purposes.
+ * It will then be replaced with a Lamda expression.
+ */
+//fun sum(value1: Int, value2: Int): Int {
+//    return value1 + value2
+//}
