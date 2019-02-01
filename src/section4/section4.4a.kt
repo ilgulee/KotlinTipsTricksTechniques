427022da2b17c370ff0f24ec6023aff0b2251e64
Ilgu Lee <iglee2000ca@gmail.com>
2019-02-01 09:37:42 -0500

    Using filter and map with collections

---
+++ src/section4/section4.4a.kt
@@ -0,0 +1,75 @@
+package section4
+
+/**
+ * Used as a template for creating a list of songs
+ */
+class MediaMetaData(
+    val id: Int = 0,
+    val title: String,
+    val artist: String,
+    val album: String
+)
+
+/**
+ * List of MediaMetaData songs.
+ */
+val musicSorce = listOf(
+    MediaMetaData(
+        100,
+        "Come Fly with Me",
+        "Frank Sinatra",
+        "Come Fly with me"
+    ),
+    MediaMetaData(
+        101,
+        "Brazil",
+        "Frank Sinatra",
+        "Come Fly with me"
+    ),
+    MediaMetaData(
+        102,
+        "Blue Hawaii",
+        "Frank Sinatra",
+        "Come Fly with me"
+    ),
+    MediaMetaData(
+        103,
+        "Suspicion",
+        "Elvis Presley",
+        "Pot Luck"
+    )
+)
+
+/**
+ * Create a function called buildPlayList(), which will
+ * build a playlist
+ */
+
+
+/**
+ * Create a function called displayPlayListTitles() to display the playlist titles
+ */
+
+
+/**
+ * Create a function called displayAlbumTitles().
+ * It will chain the filter and map calls to return the album titles
+ */
+
+
+/**
+ * This exercise will demonstrate how to execute the common collection tasks filter and map.
+ * Separate functions will be created for filtering a list by album. And then using the map
+ * command to create a new list of titles.
+ * Then a single function will created to generate the title list by chaining the filter
+ * and map commands together.
+ */
+fun main(args: Array<String>) {
+    // Call the buildPlayList() and displayPlayListTitles() first to display the titles
+    // Then replace the above two function calls with the displayAblumTitles() function.
+    val titleList = displayAlbumTitles("Come Fly with me")
+    println(titleList)
+}
+
+fun displayAlbumTitles(album: String) =
+    musicSorce.filter { it.album == album }.map { it.title }
