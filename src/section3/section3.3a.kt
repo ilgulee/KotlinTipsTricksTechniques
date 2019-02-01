366522f6016c1f514a4a94059e2eec1d15330790
Ilgu Lee <iglee2000ca@gmail.com>
2019-02-01 06:22:23 -0500

    Implementing custom behavior using field

---
+++ src/section3/section3.3a.kt
@@ -0,0 +1,25 @@
+package section3
+
+/**
+ * UseAccount class will be used to demonstrate
+ * how to create a custom accessor for the name property
+ * that will capitalize the name property.
+ */
+class UserAccount(_id: Int, _name: String) {
+    val id = _id
+    val name = _name
+        get() = field.capitalize()
+}
+
+/**
+ * This exercise will demonstrate how to add a getter custom
+ * accessor to a property. And what will happen if a backing
+ * field is not used
+ */
+fun main(args: Array<String>) {
+    // Create an instance of UserAccount
+    // Then display its contents
+    val userAccount = UserAccount(101, "ilgu")
+    println("${userAccount.name} has account id ${userAccount.id}")
+}
+
---
+++ src/section3/section3.3b.kt
@@ -0,0 +1,30 @@
+package section3
+
+/**
+ * This class will be used to apply a custom setter to
+ * results property.
+ * Which will be used to remove any 0.0 values from the property.
+ * And then add a backing field to the setter.
+ */
+class ExamResults {
+    var results: ArrayList<Double>? = null
+        set(value) {
+            value?.removeAll(arrayListOf(0.0))
+            field = value
+        }
+}
+
+/**
+ * This exercise will demonstrate how to add a setter custom
+ * accessor to a property. And what will happen if a backing
+ * field is not used
+ */
+fun main(args: Array<String>) {
+    // Create an instance of ExamResults
+    // Populate its property with scores, then display the
+    // contents of the instance
+    val scores = arrayListOf(25.0, 0.0, 77.7, 0.0, 50.0)
+    val examResults = ExamResults()
+    examResults.results = scores
+    println(examResults.results)
+}
